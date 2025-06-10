528953bba57946e3ebe441bac6b72a8ea23a6d61
Ornella <ornella@Ornellas-MacBook-Pro.local>
2025-06-10 17:05:24 +0200

    reverse word

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 // bonjour ou bonsoir
 
-// reverse.ts
-
 // Récupère l'heure actuelle
 const now = new Date();
 const hour = now.getHours();
@@ -11,4 +9,15 @@ if (hour >= 6 && hour < 18) {
 } else {
   console.log("Bonsoir !");
 }
+
+// inverse le mot
+const word = process.argv[2];
+
+if (!word) {
+  console.log("Usage: ts-node main.ts <mot>");
+  process.exit(1);
+}
+
+const reversed = word.split('').reverse().join('');
+console.log(reversed);
 
